71e93900f883ca11c07193692085e77b900504bb
randy <wiafe23@gmail.com>
2015-10-07 14:05:05 -0600

    Verison One

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	// $('#cask-beer').hide();
+	// $('h3').click(function() {
+	// 	$('#cask-beer').toggle('display');
+	// });
+$("#menu").accordion({collapsible: true, active: false});
+});
